61624ea389638843f3a5af02796fd76ac0c45fb4
Isaac Parker <parrotmac@gmail.com>
2019-05-11 17:11:52 -0700

    Move to packages; add httputils

---
+++ pkg/internal/httputils/httputils.go
@@ -0,0 +1,32 @@
+package httputils
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
+
+func RespondWithError(w http.ResponseWriter, code int, message string) {
+	RespondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func RespondWithStatus(w http.ResponseWriter, code int, message string) {
+	RespondWithJSON(w, code, map[string]string{"status": message})
+}
+
+func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, err := json.Marshal(payload)
+	if err != nil {
+		// This is (at least theoretically) dangerous as respondWithError calls this function
+		RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	_, err = w.Write(response)
+	if err != nil {
+		logrus.Errorln(err)
+	}
+}
