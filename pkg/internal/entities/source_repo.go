e5b3399073d882743bf9d61aa13c3534e942b775
Isaac Parker <parrotmac@gmail.com>
2019-05-25 16:05:28 -0700

    Remove extra tag

--- pkg/internal/entities/source_repo.go
+++ pkg/internal/entities/source_repo.go
@@ -8,6 +8,6 @@ type SourceRepository struct {
 	ID               uint `json:"id"`
 	SourceProviderID uint `json:"source_provider_id"`
 	// Gitlab is not supported -- they don't use an HMAC, only a secret https://gitlab.com/gitlab-org/gitlab-ce/issues/37380
-	AuthenticationCodeSecret string `json:"auth_code_secret"`     // HMAC secret/token
-	Name                     string `json:"name" gorm:"not null"` // e.g. "parrotmac/littleblue"
+	AuthenticationCodeSecret string `json:"auth_code_secret"` // HMAC secret/token
+	Name                     string `json:"name"`             // e.g. "parrotmac/littleblue"
 }
